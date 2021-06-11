e324267af07993f92db0923c4fdb3a1c5c26974d
Thomas Kranitsas <kranitsasthomas@gmail.com>
2021-06-11 22:04:13 +0300

    Time-based events

---
+++ app-constants.js
@@ -0,0 +1,10 @@
+/**
+ * App constants
+ */
+const EventPhaseIDs = [
+  "a93544bc-c165-4af4-b55e-18f3593b457a",
+  "6950164f-3c5e-4bdc-abc8-22aaf5a1bd49",
+  "d8a2cdbe-84d1-4687-ab75-78a6a7efdcc8",
+  "2d7d3d85-0b29-4989-b3b4-be7f2b1d0aa6"]
+
+export default EventPhaseIDs
--- handlers/events/schedule-phases.js
+++ handlers/events/schedule-phases.js
@@ -1,14 +1,14 @@
 const _ = require('lodash');
-const helper = require('../../common/helper');
+import helper from '../../common/helper';
 
 /* Responds to challenge table insert/update - schedules phases */
 export const handler = async (event, context) => {
   const challenge = await helper.getChallenge(event.detail.newImage.id)
-  if (challenge.status !== 'Active' || !_.get(challenge, 'legacy.pureV5')) {
+  if (challenge.status !== 'Active' || !_.get(challenge, 'legacy.pureV5Task')) {
     console.info(`Not creating events for challenge status ${challenge.status}...`)
     return;
   }
-  if (challenge.status === 'Active' && _.get(challenge, 'legacy.pureV5')) {
+  if (challenge.status === 'Active' && _.get(challenge, 'legacy.pureV5Task')) {
     if (event['detail-type'] && event['detail-type'] === 'INSERT') {
       // create events
       const events = helper.getEventsFromPhases(challenge);
@@ -19,13 +19,21 @@ export const handler = async (event, context) => {
     }
     if (event['detail-type'] && event['detail-type'] === 'MODIFY') {
       // create events
-      const newEvents = helper.getEventsFromPhases(challenge);
-      const oldEvents = await helper.getEventsFromScheduleApi(challenge.id)
-      console.info(`Deleting existing events for challenge ${challenge.id}`)
-      await helper.deleteEventsInExecutor(oldEvents)
-      console.info(`Creating events for challenge ${challenge.id}`)
-      await helper.createEventsInExecutor(newEvents)
-      console.info(`processing of the record completed, id: ${challenge.id}`)
+      let newEvents = helper.getEventsFromPhases(challenge);
+      let oldEvents = await helper.getEventsFromScheduleApi(challenge.id)
+      // use the scheduleTime and phases to check if there is any change
+      newEvents = _.map(newEvents, item => ({ externalId: item.externalId, scheduleTime: item.scheduleTime, payload: item.payload}))
+      oldEvents = _.map(oldEvents, item => ({ externalId: item.externalId, scheduleTime: item.scheduleTime, payload: JSON.parse(item.payload)}))
+
+      if (!_.isEqual(newEvents, oldEvents)) {
+        console.info(`Deleting existing events for challenge ${challenge.id}`)
+        await helper.deleteEventsInExecutor(oldEvents)
+        console.info(`Creating events for challenge ${challenge.id}`)
+        await helper.createEventsInExecutor(newEvents)
+        console.info(`processing of the record completed, id: ${challenge.id}`)
+      } else {
+        console.info(`No need to update events for challenge ${challenge.id}`)
+      }
       return;
     }
   }
