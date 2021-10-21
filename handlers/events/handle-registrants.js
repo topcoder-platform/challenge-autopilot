4e8d5fbe27fb045613516eabbf814091f7933120
Thomas Kranitsas <kranitsasthomas@gmail.com>
2021-10-21 11:59:02 +0300

    skip status check on tasks

--- handlers/events/handle-registrants.js
+++ handlers/events/handle-registrants.js
@@ -52,8 +52,12 @@ export const registrantsHandler = async (event, context) => {
   // Get the challenge information from the Challenge API
   const challenge = await helper.getChallenge(challengeDataFromEvent.challengeId)
 
-  if (challenge.status !== ChallengeStatuses.ACTIVE || !helper.isV5(challenge)) {
-    console.info(`The challenge ${challengeDataFromEvent.challengeId} is not Active or it's not pure V5. Skipping...`)
+  // private tasks may be assigned before activation
+  if (!helper.isV5(challenge)) {
+    console.info(`The challenge ${challengeDataFromEvent.challengeId} is not pure V5. Skipping...`)
+    return
+  } else if (challenge.status !== ChallengeStatuses.ACTIVE && !_.get(challenge, 'legacy.pureV5Task')) {
+    console.info(`The challenge ${challengeDataFromEvent.challengeId} is not Active and it's not a pure V5 task. Skipping...`)
     return
   }
 
